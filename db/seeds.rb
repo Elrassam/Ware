505933a2f3840f8fe300372558a29330cbb6b6e1
Elrassam <eng.elrassam@hotmail.com>
2013-03-06 21:33:44 +0200

    V1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+Product.create(
+	:title => 'Mohammad',
+	:description=> 'Mahmoud',
+	:image_url=> 'Elrassam',
+	:price=> 20
+)
